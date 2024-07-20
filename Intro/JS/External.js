17da4a84a73fd65e799af534394875ce5fba1e90
Shabhaizafar <zafarshabhai03.com>
2024-07-20 19:04:27 +0530

    New

---
+++ Intro/JS/External.js
@@ -0,0 +1,5 @@
+        // Console Screen 
+        console.log("Hello Everyone External" );
+
+        //main Screen 
+        document.write("Hello Everyone2 External");
---
+++ devloper Tool/JS/all2.js
@@ -0,0 +1,17 @@
+// Print Data
+// console.log("Hello");
+
+//in table 
+// console.table([11,12,14,15,13,10]);
+// console.log([11,12,14,15,13,11]);
+
+
+
+// console.time("timer : ");
+
+// console.log("Hello World");
+
+// console.timeEnd("Timer : ");
+
+
+console.log(`This is Info;color:red;`);
