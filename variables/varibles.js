1b907449dcc18476567e52dc219ab94c9387701e
Shabhaizafar <zafarshabhai03.com>
2024-07-21 18:50:10 +0530

    Variables

---
+++ variables/varibles.js
@@ -0,0 +1,84 @@
+// variable : 
+// var  a = 5;
+// a  : variable name 
+// variable : When a variable is declared in a program, the computer allocates a specific amount of memory to store the data associated with that variable.
+
+// rule of Variables  :
+
+// 1) don't start with numeric Number
+// var number1 = 12;  // valid
+// var 1number = 12;   // invalid
+
+// 2)  never use whitespace in a Variable name :
+// var numberone = 12; //valid
+// var number one = 12; //invalid
+
+// 3) never use special Char (_, $)
+// var num_1 =12;     //valid
+// var num$1 =12;    //valid
+// var num@1 =12;   //invalid
+// var num%1 = 12;  //invalid
+
+// 4) must use camelCase : 
+// var numberone = 12;  //lowercase
+// var NUMBERONE= 12;  //UPPERCASE
+// var number_one = 12;  //snake_case
+// var numberOne = 12;  //camelCase
+
+
+// 5) variable nale always unique and meaning ful
+
+// var a = 12;  //X
+// var num1=12; 
+// var age = 12;
+
+
+// 6) never use keyword as a Variable name 
+
+// var num1 = 12;
+// var var = 12;  //invalid
+
+
+
+
+
+// variable declaration : 
+// (keywords)  : reserved word
+// 1) var 
+// 2) let 
+// 3) const 
+
+
+// var a;  //declaration
+// a=10; // initialization
+
+// var a = 12;//declaration initialization
+
+
+
+// var a = 12;
+// // a=100;
+// var a = 1000;
+// console.log(a);
+
+// let b = 13;
+// // b=200;
+// // let b = 2000;
+// console.log(b);
+
+// const c = 14;
+// // c = 300;
+// // const c = 3000;
+// console.log(c);
+
+
+
+
+
+
+/*
+1) Type of Comment in JS. with example.
+2) rule of Variables.with example.
+3) what is variable ?explain with daigram.
+4) explain var,let,const.
+*/
