5e03b0d0a273c2c916324a2b269da662e520ffb7
Shabhaizafar <zafarshabhai03.com>
2024-07-22 20:57:54 +0530

    String

---
+++ String/String.js
@@ -0,0 +1,58 @@
+// - String Indexing : 
+// - it's collection of characters (single or  Multiple Character).
+// - it's similer to Array but not same.
+// - index. 
+
+
+// Double Quote
+// var str1 = "Zafar1";
+// console.log(str1,typeof str1);
+
+// // Single Quote
+// var str2 = 'Zafar2';
+// console.log(str2,typeof str2);
+
+// Back-tick
+// var str3 = `Zafar3`;
+// console.log(str3,typeof str3);
+
+
+// Multiple Line String
+// var news = `This is First Line.
+// This is Second Line.
+// `;
+// console.log(news);
+
+
+//- Template Strings.    // String Formating 
+// var fname = "abc";
+// var person = "Lazy";
+
+//Yashvi is a Lazy Girl.
+
+// console.log(fname," is a",person,"Girl.");    //X
+// console.log(fname+" is a "+person+" Girl.");  //X
+
+// console.log(`${fname} is a ${person} Girl.`);
+
+
+
+
+
+// var str = "Royal";
+// //index    01234
+// console.log(str);
+// console.log(str[0]); // similer to Array
+
+// console.log(typeof(str)==='string');
+//////////////////////////////////////
+// - Useful string methods : 
+
+
+
+
+/*
+- what is String ? Explain with Example.
+- Explain String Tamplate.
+- create a Multiline String.
+*/
