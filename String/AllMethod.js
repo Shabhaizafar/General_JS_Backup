0ef871383ea049b8b2446a83ee1b593858fbdcbe
Shabhaizafar <zafarshabhai03.com>
2024-07-28 18:48:18 +0530

    Backup

---
+++ String/AllMethod.js
@@ -0,0 +1,43 @@
+var str = "Divyaraj is a Lazy Boy.";
+console.log(str);
+
+// 1) str.charAt()   : use for which char exist specific index
+// return char/ ''
+// console.log(typeof str.charAt(100));
+
+
+// 2) str.charCodeAt(); : use for find out ASCII Value for specific index char.
+// console.log(str.charCodeAt(100));
+
+
+// 3) str.concat()
+// console.log(str.concat("It's True.","Royal"));
+
+// 4) str.startsWith()  : true/false
+// console.log(str.startsWith('Divya'));
+// console.log(str.startsWith('d'));
+
+
+// 5) str.endsWith() //true/false
+// console.log(str.endsWith('.'));
+// console.log(str.endsWith('Lazy Boy.'));
+
+
+
+// 6) str.includes() //true/false
+// console.log(str.includes('Lazy'));
+
+
+// 7) str.indexOf()
+// console.log(str.indexOf('i'));
+
+// 8) str.lastIndexOf()
+// console.log(str.lastIndexOf('i'));
+
+// 9) str.replace()
+// console.log(str.replace('i',"Hello"));
+
+
+// 10) str.replaceAll()
+// console.log(str.replaceAll('i',"Hello"));
+
