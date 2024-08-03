87ea8505692fec85864621ce3971a65cbb4f44dd
Shabhaizafar <zafarshabhai03.com>
2024-08-03 18:55:54 +0530

    Basic 2

---
+++ String/Basic2.js
@@ -0,0 +1,41 @@
+//    - Template Strings.
+// var username = "Vijay";
+// var fathername = "Rahul";
+// var lastname = "Patel";
+
+
+// console.log("My name is Raj Sharma. and My Father Name is Ajay Sharma.");
+// console.log(`My name is ${username} ${lastname}. and My Father Name is ${fathername} ${lastname}.`);
+
+
+//    - Null, undefined, BigInt, typeof.
+// var n1 = null;
+// var n2 ;
+
+// console.log(n1, typeof n1);
+// console.log(n2,typeof n2);
+
+var n1 = BigInt(12);
+console.log(n1, typeof n1);
+
+var n2 = 12345678901234567890n;
+console.log(n2,typeof n2);
+
+
+//    - Booleans and Comparison Operator.
+// console.log(1==1);
+// console.log(1=='1');
+// console.log(1===1);
+// console.log(1==='1');
+// console.log(1!=='1');
+
+
+
+
+//    - Truthy and Falsy Values.
+
+// truthy Value : 
+// true,1,-1,3.24,"sdfghjk",[],{}
+
+// // falsy value : 
+// false,0,"",undefined,null,NaN
