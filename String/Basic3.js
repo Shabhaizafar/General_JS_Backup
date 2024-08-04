24dbdf0307e3fa683a1afe73392598e52a9e98e5
Shabhaizafar <zafarshabhai03.com>
2024-08-04 18:49:23 +0530

    No Added

---
+++ String/Basic3.js
@@ -0,0 +1,41 @@
+//    - If else statement. 
+// if(condition)
+// {
+//     code;
+// }
+// else{
+//     code;
+// }
+
+var Age = 120;
+if(Age>=18)
+{
+    console.log("Adult or + ");
+}
+else{
+    console.log("Teeneger or child");
+}
+
+
+
+//    - Nested if else.
+//    - If elseif else.
+
+
+
+//    - Ternary Operator.
+
+//    - && || operator.
+
+//    - Switch statement.
+
+//    - While loop.
+//    - While loop examples.
+
+//    - For loop.
+//    - For loop examples.
+
+//    - Do while loop.
+
+
+//    - Break and continue keyword.
